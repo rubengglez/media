f2540f3dd68714cfd789e60f7a8da424d9e2e40a
Rubén García <ruben.garcia@quobis.com>
2019-11-13 21:14:58 +0100

    add book controller with new action

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,26 @@
+class BookController < ApplicationController
+    def new
+
+    end
+
+    def edit
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+
+end
